1de52a5c30dccb1f104ee216c7191d6e00dadd42
torleifg <torgravels@gmail.com>
2024-11-20 12:38:56 +0100

    Refactor database configuration and inject database name

--- src/main/kotlin/no/fdk/dataservicecatalog/config/MongoConfig.kt
+++ src/main/kotlin/no/fdk/dataservicecatalog/config/MongoConfig.kt
@@ -1,6 +1,7 @@
 package no.fdk.dataservicecatalog.config
 
 import com.mongodb.client.MongoClient
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.config.EnableMongoAuditing
@@ -11,8 +12,11 @@ import org.springframework.data.mongodb.core.MongoTemplate
 @EnableMongoAuditing
 class MongoConfig {
 
+    @Value("\${spring.data.mongodb.database}")
+    lateinit var database: String
+
     @Bean
     fun mongoTemplate(mongoClient: MongoClient): MongoOperations {
-        return MongoTemplate(mongoClient, "dataServiceCatalog")
+        return MongoTemplate(mongoClient, database)
     }
 }
