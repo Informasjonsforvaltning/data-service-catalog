60c908e814bbd3df7ea07f9aa61046707b3da0da
torleifg <torgravels@gmail.com>
2024-11-27 15:49:38 +0100

    Initial RDF serialization

--- src/main/kotlin/no/fdk/dataservicecatalog/handler/RDFHandler.kt
+++ src/main/kotlin/no/fdk/dataservicecatalog/handler/RDFHandler.kt
@@ -1,14 +1,37 @@
 package no.fdk.dataservicecatalog.handler
 
+import no.fdk.dataservicecatalog.domain.DataService
+import no.fdk.dataservicecatalog.domain.Status
 import no.fdk.dataservicecatalog.repository.DataServiceRepository
+import org.apache.jena.rdf.model.Model
+import org.apache.jena.rdf.model.ModelFactory
+import org.apache.jena.rdf.model.ResourceFactory
 import org.apache.jena.riot.Lang
+import org.apache.jena.sparql.vocabulary.FOAF
+import org.apache.jena.util.URIref
+import org.apache.jena.vocabulary.*
 import org.springframework.stereotype.Component
+import java.io.StringWriter
 
 @Component
 class RDFHandler(private val repository: DataServiceRepository) {
 
     fun findAll(lang: Lang): String {
-        return ""
+        val dataServices = repository.findAllByStatus(Status.PUBLISHED)
+
+        if (dataServices.isEmpty()) {
+            return ""
+        }
+
+        val model = createModel()
+
+        val dataServicesByCatalogId = dataServices.groupBy(DataService::catalogId)
+
+        for (entry in dataServicesByCatalogId) {
+            model.addCatalog(entry.key!!)
+        }
+
+        return model.serialise(lang)
     }
 
     fun findById(catalogId: String, lang: Lang): String {
@@ -18,4 +41,37 @@ class RDFHandler(private val repository: DataServiceRepository) {
     fun findById(catalogId: String, dataServiceId: String, lang: Lang): String {
         return ""
     }
+
+    private fun createModel(): Model {
+        return ModelFactory
+            .createDefaultModel()
+            .setNsPrefixes(
+                mapOf(
+                    "dcat" to DCAT.NS,
+                    "dct" to DCTerms.NS,
+                    "rdf" to RDF.uri,
+                    "vcard" to VCARD4.NS,
+                    "foaf" to FOAF.NS
+                )
+            )
+    }
+}
+
+fun Model.addCatalog(catalogId: String) {
+    this.createResource(URIref.encode("/catalogs/".plus(catalogId)))
+        .addProperty(RDF.type, DCAT.Catalog)
+        .addProperty(DCTerms.publisher, ResourceFactory.createResource(URIref.encode(catalogId)))
+        .addProperty(DCTerms.title, ResourceFactory.createLangLiteral("Data service catalog ($catalogId)", "en"))
+
+    this.createResource(URIref.encode("/organizations/".plus(catalogId)))
+        .addProperty(RDF.type, FOAF.Agent)
+        .addProperty(DCTerms.identifier, catalogId)
+        .addProperty(OWL.sameAs, URIref.encode(catalogId))
+}
+
+fun Model.serialise(lang: Lang): String {
+    val stringWriter = StringWriter()
+    this.write(stringWriter, lang.name)
+
+    return stringWriter.buffer.toString()
 }
--- src/test/kotlin/no/fdk/dataservicecatalog/unit/handler/RDFHandlerTest.kt
+++ src/test/kotlin/no/fdk/dataservicecatalog/unit/handler/RDFHandlerTest.kt
@@ -1,5 +1,7 @@
 package no.fdk.dataservicecatalog.unit.handler
 
+import no.fdk.dataservicecatalog.domain.DataService
+import no.fdk.dataservicecatalog.domain.Status
 import no.fdk.dataservicecatalog.handler.RDFHandler
 import no.fdk.dataservicecatalog.repository.DataServiceRepository
 import org.apache.jena.riot.Lang
@@ -9,6 +11,8 @@ import org.junit.jupiter.api.extension.ExtendWith
 import org.mockito.InjectMocks
 import org.mockito.Mock
 import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.stub
 
 @ExtendWith(MockitoExtension::class)
 class RDFHandlerTest {
@@ -21,7 +25,38 @@ class RDFHandlerTest {
 
     @Test
     fun `find should respond with empty string`() {
+        repository.stub {
+            on { findAllByStatus(Status.PUBLISHED) } doReturn emptyList()
+        }
+
         assertEquals("", handler.findAll(Lang.TURTLE))
+    }
+
+    @Test
+    fun `find should respond with turtle string`() {
+        val turtle = """
+            PREFIX dcat:  <http://www.w3.org/ns/dcat#>
+            PREFIX dct:   <http://purl.org/dc/terms/>
+            PREFIX foaf:  <http://xmlns.com/foaf/0.1/>
+            PREFIX rdf:   <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+            PREFIX vcard: <http://www.w3.org/2006/vcard/ns#>
+            
+            </catalogs/1234>  rdf:type  dcat:Catalog;
+                    dct:publisher  <1234>;
+                    dct:title      "Data service catalog (1234)"@en .
+
+            </organizations/1234>
+                    rdf:type        foaf:Agent;
+                    dct:identifier  "1234";
+                    <http://www.w3.org/2002/07/owl#sameAs>
+                            "1234" .
+        """
+
+        repository.stub {
+            on { findAllByStatus(Status.PUBLISHED) } doReturn listOf(DataService(catalogId = "1234"))
+        }
+
+        assertEquals(turtle.trimIndent(), handler.findAll(Lang.TURTLE).trimEnd())
     }
 
     @Test
