f42b5d31d281f4057aac945b06daf11c6a935acd
Torleif Gravelsæter <torgravels@gmail.com>
2024-12-10 08:39:56 +0100

    Refactor security and add audit on modified by (#22)

--- src/main/kotlin/no/fdk/dataservicecatalog/config/MongoConfig.kt
+++ src/main/kotlin/no/fdk/dataservicecatalog/config/MongoConfig.kt
@@ -2,20 +2,27 @@ package no.fdk.dataservicecatalog.config
 
 import com.mongodb.client.MongoClient
 import no.fdk.dataservicecatalog.domain.DataService
+import no.fdk.dataservicecatalog.domain.User
 import org.bson.Document
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.data.domain.AuditorAware
 import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.config.EnableMongoAuditing
 import org.springframework.data.mongodb.core.MongoOperations
 import org.springframework.data.mongodb.core.MongoTemplate
 import org.springframework.data.mongodb.core.index.CompoundIndexDefinition
 import org.springframework.data.mongodb.core.index.Index
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
+import java.util.*
 
 @Configuration
 @EnableMongoAuditing
-class MongoConfig(@Value("\${spring.data.mongodb.database}") private val database: String) {
+class MongoConfig(@Value("\${spring.data.mongodb.database}") private val database: String) : AuditorAware<User> {
 
     @Bean
     fun mongoTemplate(mongoClient: MongoClient): MongoOperations {
@@ -32,4 +39,27 @@ class MongoConfig(@Value("\${spring.data.mongodb.database}") private val databas
 
         return true
     }
+
+    override fun getCurrentAuditor(): Optional<User> {
+        val user = SecurityContextHolder.getContext()
+            ?.authentication
+            ?.takeIf { it.isAuthenticated }
+            ?.let { it.principal as? Jwt }
+            ?.let { jwt ->
+                jwt.claims["user_name"]
+                    ?.let { it as? String }
+                    .also { if (it == null) logger.error("user_name claim missing in token") }
+                    ?.let { userName ->
+                        User(
+                            id = userName,
+                            name = jwt.claims["name"] as? String,
+                            email = jwt.claims["email"] as? String,
+                        )
+                    }
+            }
+
+        return Optional.ofNullable(user)
+    }
 }
+
+private val logger: Logger = LoggerFactory.getLogger(MongoConfig::class.java)
---
+++ src/test/kotlin/no/fdk/dataservicecatalog/unit/config/MongoConfigTest.kt
@@ -0,0 +1,99 @@
+package no.fdk.dataservicecatalog.unit.config
+
+import no.fdk.dataservicecatalog.config.MongoConfig
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.mockito.Mock
+import org.mockito.junit.jupiter.MockitoExtension
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.stub
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.jwt.Jwt
+
+@ExtendWith(MockitoExtension::class)
+class MongoConfigTest {
+
+    @Mock
+    lateinit var securityContext: SecurityContext
+
+    @Mock
+    lateinit var authentication: Authentication
+
+    @Mock
+    lateinit var jwt: Jwt
+
+    private lateinit var config: MongoConfig
+
+    @BeforeEach
+    fun setup() {
+        SecurityContextHolder.setContext(securityContext)
+
+        config = MongoConfig("database")
+    }
+
+    @Test
+    fun `should return user from JWT`() {
+        securityContext.stub {
+            on { securityContext.authentication } doReturn authentication
+        }
+
+        authentication.stub {
+            on { isAuthenticated } doReturn true
+            on { principal } doReturn jwt
+        }
+
+        jwt.stub {
+            on { claims } doReturn mapOf(
+                "user_name" to "user_name",
+                "name" to "name",
+                "email" to "email"
+            )
+        }
+
+        val auditor = config.getCurrentAuditor()
+
+        assertTrue(auditor.isPresent)
+        val user = auditor.get()
+
+        assertEquals("user_name", user.id)
+        assertEquals("name", user.name)
+        assertEquals("email", user.email)
+    }
+
+
+    @Test
+    fun `should return empty on missing authentication`() {
+        securityContext.stub {
+            on { authentication } doReturn null
+        }
+
+        val auditor = config.getCurrentAuditor()
+
+        assertTrue(auditor.isEmpty)
+    }
+
+    @Test
+    fun `should return empty on missing user_name claim`() {
+        securityContext.stub {
+            on { securityContext.authentication } doReturn authentication
+        }
+
+        authentication.stub {
+            on { isAuthenticated } doReturn true
+            on { principal } doReturn jwt
+        }
+
+        jwt.stub {
+            on { claims } doReturn emptyMap()
+        }
+
+        val auditor = config.getCurrentAuditor()
+
+        assertTrue(auditor.isEmpty)
+    }
+}
